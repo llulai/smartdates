aa328078417be11b7865d210f254ba555d97fe75
llulai <oilujulio@gmail.com>
2018-03-30 15:59:30 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,33 @@
+Smart Dates
+===============================
+
+version number: 0.0.1
+author: Julio Gajardo
+
+Overview
+--------
+
+Query dates in a smart way
+
+Installation / Usage
+--------------------
+
+To install use pip:
+
+    $ pip install smartdates
+
+
+Or clone the repo:
+
+    $ git clone https://github.com/llulai/smartdates.git
+    $ python setup.py install
+    
+Contributing
+------------
+
+TBD
+
+Example
+-------
+
+TBD
---
+++ setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+__version__ = '0.0.1'
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+# get the dependencies and installs
+with open(path.join(here, 'requirements.txt'), encoding='utf-8') as f:
+    all_reqs = f.read().split('\n')
+
+install_requires = [x.strip() for x in all_reqs if 'git+' not in x]
+dependency_links = [x.strip().replace('git+', '') for x in all_reqs if x.startswith('git+')]
+
+setup(
+    name='smartdates',
+    version=__version__,
+    description='Query dates in a smart way',
+    long_description=long_description,
+    url='https://github.com/llulai/smartdates',
+    download_url='https://github.com/llulai/smartdates/tarball/' + __version__,
+    license='BSD',
+    classifiers=[
+      'Development Status :: 3 - Alpha',
+      'Intended Audience :: Developers',
+      'Programming Language :: Python :: 3',
+    ],
+    keywords='',
+    packages=find_packages(exclude=['docs', 'tests*']),
+    include_package_data=True,
+    author='Julio Gajardo',
+    install_requires=install_requires,
+    dependency_links=dependency_links,
+    author_email='oilujulio@gmail.com'
+)
